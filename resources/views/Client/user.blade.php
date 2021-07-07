cdeb47a83d8049e7ffda96b8965c011ebbf7528c
lemeyou <58194255+lemeyou@users.noreply.github.com>
2021-07-07 15:04:14 +0100

    modif

--- resources/views/Client/user.blade.php
+++ resources/views/Client/user.blade.php
@@ -1,5 +1,74 @@
 @extends('Client.layout.default')
 @section('title', 'User')
+@section ('nav')
+
+        <nav class="nav">
+            <div>
+                <a href="#" class="nav_logo">
+                    <i class='bx bx-layer nav_logo-icon'></i>
+                    <span class="nav_logo-name">
+                        Christian Kepya
+                    </span>
+                </a>
+
+                <div class="nav_list">
+                    <a href="/home" class="nav_link" data-bs-toggle="tooltip" data-bs-placement="right" title="Home">
+                        <i class='bx bx-grid-alt nav_icon'></i>
+                        <span class="nav_name">Home</span>
+                    </a>
+
+                    <a href="/user" class="nav_link active" data-bs-toggle="tooltip" data-bs-placement="right" title="Profile">
+                        <i class='bx bx-user nav_icon'></i>
+                        <span class="nav_name">User</span>
+                    </a>
+                    
+                    <a href="/invoice" class="nav_link" data-bs-toggle="tooltip" data-bs-placement="right" title="Bills">
+                        <i class='fas fa-file-invoice-dollar nav_icon'></i>
+                        <span class="nav_name">Invoice</span>
+                    </a>
+                    
+                    <a href="/receipt" class="nav_link" data-bs-toggle="tooltip" data-bs-placement="right" title="Receipt">
+                        <i class='fas fa-receipt nav_icon'></i>
+                        <span class="nav_name">Receipt</span>
+                    </a>
+
+                    <a href="/message" class="nav_link" data-bs-toggle="tooltip" data-bs-placement="right" title="Message">
+                        <i class='bx bx-message-square-detail nav_icon'></i>
+                        <span class="nav_name">Messages</span>
+                    </a>
+                </div>
+            </div>
+
+            <section class="nav_notify_button" id="notify">
+                <i class='bx bx-bell nav_icon not'>
+                    <i class='bx bx-radio-circle bx-burst nav_notify_radio_position' style='color:#ffe200' id="bx1"></i>
+                    <i class='bx bxs-circle nav_notify_circle_position' style='color:#ffe200' id="bx"></i>
+                </i>
+                <span class="nav_name not" id="not">Notification</span>
+            </section>
+
+            <section class="hide_menu_account card" id="sms">
+                <section class="card-header espace">
+                    <span class="font-12 col-xs-6 font-semi-bold mr-4">Nouvelles notifications</span>
+                    <a class="mark-notification-read col-xs-6 text-right font-12 font-semi-bold" href="javascript:;"> Marquer comme lu</a>
+                </section>
+                <a href="google.com" class="noti pl-4 pr-4 pt-3 pb-3">
+                    <div class="user_i"><i class='bx bx-user bxUser'></i></div>
+                    <div class="forage">
+                        <div>Bienvenue sur ForageManager</div>
+                        <div style="font-size: 12px;">il y'a deux minute</div>
+                    </div>
+                </a>
+                <hr>
+            </section>
+
+            <a href="#" class="nav_link">
+                <i class='bx bx-log-out nav_icon'></i>
+                <span class="nav_name">Log out</span>
+            </a>
+        </nav>
+
+@stop
 @section('main')
 
 <h1>                            
