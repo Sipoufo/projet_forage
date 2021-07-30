e11f1ed4c023e0c33b3ef2d502d03356cd834f3c
Sipoufo <sipoufoknj@gmail.com>
2021-07-30 20:27:41 +0100

    feat

--- resources/views/admin/maps.blade.php
+++ resources/views/admin/maps.blade.php
@@ -208,12 +208,14 @@ crossorigin=""/><!-- Make sure you put this AFTER Leaflet's CSS -->
         });
         var adminIcon = L.icon({
             iconUrl: '/img/pointeur1.png',
-            iconSize:     [32, 32], 
-            iconAnchor:   [22, 94], 
-            popupAnchor:  [-3, -76] 
+            iconSize:     [38, 95], // size of the icon
+            shadowSize:   [50, 64], // size of the shadow
+            iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+            shadowAnchor: [4, 62],  // the same for the shadow
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
         });
 
-        L.marker([admin.localisation.longitude, admin.localisation.latitude], {icon: adminIcon}).addTo(mymap).bindPopup("<h4>Admin</h4>").openPopup();
+        L.marker([admin.localisation.latitude, admin.localisation.longitude], {icon: adminIcon}).addTo(mymap).bindPopup("<h4>Admin</h4>").openPopup();
         
         for (let i = 0; i < NumClient; i++) {
             if (client[i].localisation.latitude && client[i].localisation.longitude) {
@@ -224,7 +226,7 @@ crossorigin=""/><!-- Make sure you put this AFTER Leaflet's CSS -->
                         return client[i].localisation.description
                     }
                 }
-                L.marker([client[i].localisation.longitude, client[i].localisation.latitude], {icon: greenIcon}).addTo(mymap).bindPopup(client[i].localisation.description).openPopup();
+                L.marker([client[i].localisation.longitude, client[i].localisation.latitude]).addTo(mymap).bindPopup(client[i].localisation.description).openPopup();
             }
             
         }
