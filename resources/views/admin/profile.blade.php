a7cbff09aba848294516c0bfb6b6f46f1bd6e5eb
Bryan Fenyom <bryanfenyom@gmail.com>
2022-03-07 09:44:34 +0100

    new

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -252,24 +252,6 @@
                                 </div>
                               </div>
                               <hr>
-                              <div class="row">
-                                <div class="col-sm-3">
-                                  <h6 class="mb-0">Registered at</h6>
-                                </div>
-                                <div class="col-sm-9 text-secondary">
-                                  <?= date('d-m-Y H:i:s', strtotime($data['createdAt']))?>
-                                </div>
-                              </div>
-                              <hr>
-                              <div class="row">
-                                <div class="col-sm-3">
-                                  <h6 class="mb-0">Updated at</h6>
-                                </div>
-                                <div class="col-sm-9 text-secondary">
-                                  <?= date('d-m-Y H:i:s', strtotime($data['updatedAt']))?>
-                                </div>
-                              </div>
-                              <hr>
                           </div>
 
 
